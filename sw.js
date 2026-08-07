097ddd2cca001977d94991fe6667f66d640a097f
swar9622-svg <swar9622@gmail.com>
2026-08-07 03:48:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,34 @@
-const CACHE_NAME = 'attendance-app-v3';
+const CACHE_NAME = 'attendance-app-v4';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon.png'
+  './icon.png',
+  './html2pdf.bundle.min.js'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
